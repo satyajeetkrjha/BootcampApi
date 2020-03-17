0d2ce1b31be4a65bda397eb11d306c65ed11d3cd
satyajeet <satyajeet@getvital.in>
2020-03-18 03:45:56 +0530

    proper error logging

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,72 +1,66 @@
-
 const Bootcamp = require('../models/Bootcamp');
-const colors = require('colors')
+const ErrorResponse = require('../utils/errorResponse');
+
+const colors = require('colors');
 // Get all bootcamps
 // GET /api/v1/bootcamps
 //@acess public
-exports.getBootcamps = async (req,res,next) =>{
-    try{
-        const bootcamp = await Bootcamp.find() ;// gets all the data from this collection
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.find();// gets all the data from this collection
         res.status(200).json({
-            success:true,
-            count:bootcamp.length,
-            data:bootcamp
+            success: true,
+            count: bootcamp.length,
+            data: bootcamp
         })
-    }catch (e) {
+    } catch (e) {
         res.status(400).json({
-            success:false,
-            error:e
+            success: false,
+            error: e
         })
     }
 
-}
+};
 
 // Get a single  bootcamp
 // GET /api/v1/bootcamps/:id
 //@acess public 
 exports.getBootcamp = async (req,res,next) =>{
-    try{
-        const bootcamp = await  Bootcamp.findById(req.params.id)
-        if(!bootcamp){
-          return   res.status(400).json({
-                success:false,
-                msg:'This id does not exist'
-            })
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+        if (!bootcamp) {
+            return next(new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`, 404))
         }
 
         res.status(200).json({
-            success:true,
-            data:bootcamp
+            success: true,
+            data: bootcamp
         })
-    }catch (e) {
-        next(e)
-       // res.json({
-       //     success:false,
-       //     error:e
-       // })
+    } catch (e) {
+        next(new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`, 404))
     }
 
-}
+};
 
 // Post  Create new bootcamp
 // POST /api/v1/bootcamps
 //@acess private
 exports.createBootcamp = async (req,res,next) =>{
-    try{
+    try {
         const bootcamp = await Bootcamp.create(req.body);
         res.status(201).json({
-            success:true,
-            data:bootcamp
+            success: true,
+            data: bootcamp
         });
-    }catch (e) {
-       console.log(colors.red(e)) ;
-       res.status(400).json({
-           success:false,
-           error:e
-       });
+    } catch (e) {
+        console.log(colors.red(e));
+        res.status(400).json({
+            success: false,
+            error: e
+        });
     }
 
-}
+};
 
 // PUT  Update bootcamp
 // PUT /api/v1/bootcamp/:id
@@ -77,26 +71,26 @@ exports.updateBootcamp = async (req,res,next) =>{
             new:true,
             runValidators:true
         });
-        if(!bootcamp){
+        if (!bootcamp) {
             return res.status(400).json({
-                success:false,
-                msg:'This id does not exist and cannot be updated'
+                success: false,
+                msg: 'This id does not exist and cannot be updated'
             })
         }
         res.status(200).json({
-            data:bootcamp,
-            success:true
+            data: bootcamp,
+            success: true
         })
-    }catch (e) {
-        console.log(colors.red(e)) ;
+    } catch (e) {
+        console.log(colors.red(e));
         res.status(400).json({
-            success:false,
-            error:e
+            success: false,
+            error: e
         });
     }
 
 
-}
+};
 
 //DELETE Delete Bootcamp
 //DELETE /api/v1/bootcamp/:id
@@ -106,22 +100,22 @@ exports.deleteBootcamp = async (req,res,next) =>{
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
         if(!bootcamp){
-          return  res.status(200).json({
-                success:true,
-                msg:'This id does not exist'
+            return res.status(200).json({
+                success: true,
+                msg: 'This id does not exist'
             })
         }
         res.status(200).json({
-            success:true,
-            msg:'Deleted '
+            success: true,
+            msg: 'Deleted '
         })
 
-    }catch (e) {
-        console.log(colors.red(e)) ;
+    } catch (e) {
+        console.log(colors.red(e));
         res.status(400).json({
-            success:false,
-            error:e
+            success: false,
+            error: e
         });
     }
 
-}
+};
--- middleware/error.js
+++ middleware/error.js
@@ -1,11 +1,10 @@
-const colors = require('colors')
-const errorHandler = (err, req,res,next) =>{
+const errorHandler = (err, req, res, next) => {
     // mmost important line on this planet if you don't want to die
-    console.log(err.stack.red)
-    res.status(500).json({
-        success:false,
-        msg:err.message
+    console.log(err.stack.red);
+    res.status(err.statusCode || 500).json({
+        success: false,
+        msg: err.message || 'Server Error'
     })
 
-}
-module.exports=errorHandler;
+};
+module.exports = errorHandler;
