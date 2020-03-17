3f894d4624af4b1fa87bc7fa47312a0ce9903957
satyajeet <satyajeet@getvital.in>
2020-03-18 02:21:07 +0530

    getting a single bootcamp with try and catch block etc

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -4,15 +4,45 @@ const colors = require('colors')
 // Get all bootcamps
 // GET /api/v1/bootcamps
 //@acess public
-exports.getBootcamps =(req,res,next) =>{
-    res.status(200).json({success:true,msg:'Show all bootcamps',hello:req.hello});
+exports.getBootcamps = async (req,res,next) =>{
+    try{
+        const bootcamp = await Bootcamp.find() ;// gets all the data from this collection
+        res.status(200).json({
+            success:true,
+            data:bootcamp
+        })
+    }catch (e) {
+        res.status(400).json({
+            success:false,
+            error:e
+        })
+    }
+
 }
 
 // Get a single  bootcamp
 // GET /api/v1/bootcamps/:id
-//@acess public
-exports.getBootcamp =(req,res,next) =>{
+//@acess public 
+exports.getBootcamp = async (req,res,next) =>{
+    try{
+        const bootcamp = await  Bootcamp.findById(req.params.id)
+        if(!bootcamp){
+          return   res.status(400).json({
+                success:false,
+                msg:'This id does not exist'
+            })
+        }
 
+        res.status(200).json({
+            success:true,
+            data:bootcamp
+        })
+    }catch (e) {
+       res.json({
+           success:false,
+           error:e
+       })
+    }
     res.status(200).json({ success:true,msg:`Get a bootcamp with ${req.params.id}`});
 }
 
