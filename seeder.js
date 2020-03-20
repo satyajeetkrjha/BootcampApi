32e359d7faa55678b325f60a0d4a7ec9251a075b
satyajeet <satyajeet@getvital.in>
2020-03-21 02:56:56 +0530

    dumping data into database with a seeder using process args

---
+++ seeder.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+
+require('dotenv').config();
+
+//We load the model
+const Bootcamp = require('./models/Bootcamp');
+
+//Now connect to Database
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+
+//We will read json file ,parse it and dump it in database
+const bootcamps = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+//Dumping into database
+const importData = async () => {
+    try {
+        //Dumped
+        await Bootcamp.create(bootcamps);
+        console.log('Data dumped into database'.green);
+        process.exit();// kill te server
+    } catch (err) {
+        console.log("err is ", err)
+    }
+};
+//Deleting from database
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+        console.log("data deleted");
+        proces.exit();
+    } catch (err) {
+        console.log("err in deleting data", err);
+    }
+};
+
+
+if (process.argv[2] === '-i') {
+    importData();
+} else if (process.argv[2] === '-d') {
+    deleteData();
+}
+
